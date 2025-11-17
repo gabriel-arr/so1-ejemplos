c6ef15b34e5c57738fd0d858b4e33f0fb6ac109c
Gabriel <gabriel.arrieta@mi.unc.edu.ar>
2025-11-17 20:55:27 -0300

    Enhance comments in pipe_with_fork.c for clarity
    Added comments to clarify pipe behavior and process execution.

--- ipc/pipe/pipe_with_fork.c
+++ ipc/pipe/pipe_with_fork.c
@@ -10,11 +10,13 @@
 int main ()
 {
     char *s, buf[1024];
-    int fds[2];
+    int fds[2]; //el kernel se encarga de hacer la asociacion para que pasandole un arreglo depende de cual posicion se tenga lee o escribe
 
     pipe(fds);
 
-    if (fork() == 0) {
+    if (fork() == 0) { 
+    //aca solo entra el hijo, mientras que el padre tranquilamente puede estar ejecutandose concurrentemente
+    //con el codigo fuera de este if
         printf("child\n");
         
         s = "hello world\n";
@@ -24,7 +26,9 @@ int main ()
     }
 
     printf("father will block until read\n");
-    
+
+    //el read es bloqueante hasta que se termine de escribir entonces hasta que el proceso hijo
+    //termine de escribir el proceso padre estara bloqueado 
     read(fds[0], buf, 12);
     printf("father continues..\n");
 
